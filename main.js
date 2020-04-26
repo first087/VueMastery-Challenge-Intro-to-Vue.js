39067de3742a71d53ff5c9e58eea7edcedd3d1ea
Artit Kiuwilai <first087@gmail.com>
2020-04-26 22:31:38 +0700

    Init Challenge 9 - Communicating Events

--- main.js
+++ main.js
@@ -1,18 +1,4 @@
-//Create a new component for product-details with a prop of details. 
-
-Vue.component('product-details', {
-  props: {
-    details: {
-      type: Array,
-      required: true
-    }
-  },
-  template: `
-    <ul>
-      <li v-for="detail in details">{{ detail }}</li>
-    </ul>
-  `
-})
+//Add a button that removes the product from the cart array by emitting an event with the id of the product to be removed.
 
 Vue.component('product', {
   props: {
@@ -22,43 +8,41 @@ Vue.component('product', {
     }
   },
   template: `
-   <div class="product">
-        
-      <div class="product-image">
-        <img :src="image" />
+     <div class="product">
+          
+        <div class="product-image">
+          <img :src="image" />
+        </div>
+  
+        <div class="product-info">
+            <h1>{{ product }}</h1>
+            <p v-if="inStock">In Stock</p>
+            <p v-else>Out of Stock</p>
+            <p>Shipping: {{ shipping }}</p>
+  
+            <ul>
+              <li v-for="detail in details">{{ detail }}</li>
+            </ul>
+  
+            <div class="color-box"
+                 v-for="(variant, index) in variants" 
+                 :key="variant.variantId"
+                 :style="{ backgroundColor: variant.variantColor }"
+                 @mouseover="updateProduct(index)"
+                 >
+            </div> 
+  
+            <button v-on:click="addToCart" 
+              :disabled="!inStock"
+              :class="{ disabledButton: !inStock }"
+              >
+            Add to cart
+            </button>
+  
+         </div>  
+      
       </div>
-
-      <div class="product-info">
-          <h1>{{ product }}</h1>
-          <p v-if="inStock">In Stock</p>
-          <p v-else>Out of Stock</p>
-          <p>Shipping: {{ shipping }}</p>
-
-          <product-details :details="details"></product-details>
-
-          <div class="color-box"
-               v-for="(variant, index) in variants" 
-               :key="variant.variantId"
-               :style="{ backgroundColor: variant.variantColor }"
-               @mouseover="updateProduct(index)"
-               >
-          </div> 
-
-          <button v-on:click="addToCart" 
-            :disabled="!inStock"
-            :class="{ disabledButton: !inStock }"
-            >
-          Add to cart
-          </button>
-
-          <div class="cart">
-            <p>Cart({{ cart }})</p>
-          </div>
-
-       </div>  
-    
-    </div>
-   `,
+     `,
   data() {
     return {
       product: 'Socks',
@@ -78,13 +62,12 @@ Vue.component('product', {
           variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg',
           variantQuantity: 0
         }
-      ],
-      cart: 0
+      ]
     }
   },
   methods: {
     addToCart: function () {
-      this.cart += 1
+      this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId)
     },
     updateProduct: function (index) {
       this.selectedVariant = index
@@ -112,6 +95,12 @@ Vue.component('product', {
 var app = new Vue({
   el: '#app',
   data: {
-    premium: true
+    premium: true,
+    cart: []
+  },
+  methods: {
+    updateCart(id) {
+      this.cart.push(id)
+    }
   }
 })
