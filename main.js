a10b36cd00da2cb3b12638b88ccfbf74229bc0c5
Artit Kiuwilai <first087@gmail.com>
2020-04-26 19:36:29 +0700

    Init Challenge 7 - Computed Properties

--- main.js
+++ main.js
@@ -1,32 +1,46 @@
-//When inStock is false, bind a class to the “Out of Stock” p tag that adds  text-decoration: line-through to that element.
+//Add a new boolean data property `onSale` and create a computed property that takes `brand`, `product` and `onSale` and prints out a string whenever `onSale` is true.
 
 var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    image: './assets/vmSocks-green.jpg',
-    inStock: false,
+    brand: 'Vue Mastery',
+    selectedVariant: 0,
     details: ['80% cotton', '20% polyester', 'Gender-neutral'],
     variants: [
       {
         variantId: 2234,
         variantColor: 'green',
-        variantImage: './assets/vmSocks-green.jpg'
+        variantImage: './assets/vmSocks-green.jpg',
+        variantQuantity: 10
       },
       {
         variantId: 2235,
         variantColor: 'blue',
-        variantImage: './assets/vmSocks-blue.jpg'
+        variantImage: './assets/vmSocks-blue.jpg',
+        variantQuantity: 0
       }
     ],
-    cart: 0
+    cart: 0,
   },
   methods: {
     addToCart() {
       this.cart += 1
     },
-    updateProduct(variantImage) {
-      this.image = variantImage
+    updateProduct(index) {
+      this.selectedVariant = index
+      console.log(index)
+    }
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity
     },
   }
 })
